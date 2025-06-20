c6904c3fe94948d5060c8a47f20124187a335772
UpDev <84450789+updevv@users.noreply.github.com>
2025-06-21 01:14:16 +0200

    Add files via upload

---
+++ updev_location/client/client.lua
@@ -0,0 +1,136 @@
+local openmenu = false
+local locationBlips = {}
+local rentedVehicles = {}
+
+function OpenLocationMenu(coords, heading)
+    local menuPrincipal = RageUI.CreateMenu(nil, "Gérer les shops", nil, nil)
+
+    if openmenu then
+        openmenu = false
+        RageUI.Visible(menuPrincipal, false)
+    else
+        openmenu = true
+        RageUI.Visible(menuPrincipal, true)
+        CreateThread(function()
+            while openmenu do
+                RageUI.IsVisible(menuPrincipal, function()
+                    RageUI.Separator("Location de véhicule")
+                    RageUI.Line()
+                    for _, v in pairs(Config.LocationVehicle) do
+                        RageUI.Button(v.label, "Appuyez sur le bouton pour louer le véhicule", { RightLabel = v.price .. "$" }, true, {
+                            onSelected = function()
+                                local alert = lib.alertDialog({
+                                    header = 'Confirmer la location',
+                                    content = 'Voulez-vous vraiment louer ce véhicule ?',
+                                    centered = true,
+                                    cancel = true
+                                })
+                                if alert == "confirm" then
+                                    ESX.TriggerServerCallback("updev:location", function(success, vehicleId)
+                                        if success then
+                                            RageUI.CloseAll()
+                                            openmenu = false
+                                    
+                                            local vehicle = NetworkGetEntityFromNetworkId(vehicleId)
+                                            rentedVehicles[vehicleId] = {
+                                                vehicle = vehicleId,
+                                                rentTime = GetGameTimer()
+                                            }
+                                            
+                                            CreateThread(function()
+                                                while rentedVehicles[vehicleId] do
+                                                    Wait(0)
+                                                    local playerPed = PlayerPedId()
+                                                    local playerVeh = GetVehiclePedIsIn(playerPed, false)
+                                                    
+                                                    if DoesEntityExist(vehicle) and playerVeh == vehicle then
+                                                        local remainingTime = 900000 - (GetGameTimer() - rentedVehicles[vehicleId].rentTime)
+                                                        
+                                                        if remainingTime <= 0 then
+                                                            ESX.ShowNotification("⏰ Le temps de location est écoulé. Le véhicule va être supprimé.")
+                                                            if DoesEntityExist(vehicle) then
+                                                                DeleteEntity(vehicle)
+                                                            end
+                                                            rentedVehicles[vehicleId] = nil
+                                                            break
+                                                        else
+                                                            local minutes = math.floor(remainingTime / 60000)
+                                                            local seconds = math.floor((remainingTime % 60000) / 1000)
+                                                            local timeText = ("~HUD_COLOUR_PM_WEAPONS_PURCHASABLE~Temps restant: %02d:%02d"):format(minutes, seconds)
+                                                            DrawTextFixed(0.9, 0.82, timeText)
+                                                        end
+                                                    elseif not DoesEntityExist(vehicle) then
+                                                        rentedVehicles[vehicleId] = nil
+                                                        break
+                                                    end
+                                                end
+                                            end)
+                                        end
+                                    end, v.model, v.price, v.label, coords, heading, Config.LocationTime)
+                                end
+                            end
+                        })
+                    end
+                end)
+
+                Wait(0)
+            end
+        end)
+    end
+end
+
+CreateThread(function()
+    for _, location in pairs(Config.LocationCoords) do
+        local blip = AddBlipForCoord(location.coords.x, location.coords.y, location.coords.z)
+        SetBlipSprite(blip, 280)
+        SetBlipDisplay(blip, 4)
+        SetBlipScale(blip, 0.5)
+        SetBlipColour(blip, 2)
+        SetBlipAsShortRange(blip, true)
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentSubstringPlayerName("Location de véhicule")
+        EndTextCommandSetBlipName(blip)
+    end
+end)
+
+CreateThread(function()
+    while true do
+        local sleep = 500
+        local playerPed = PlayerPedId()
+        local playerCoords = GetEntityCoords(playerPed)
+
+        for _, location in pairs(Config.LocationCoords) do
+            local distance = #(playerCoords - vector3(location.coords.x, location.coords.y, location.coords.z))
+
+            if distance <= 6.0 then
+                sleep = 0
+
+                DrawMarker(25, location.coords.x, location.coords.y, location.coords.z - 0.98, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.55, 0.55, 0.55, 0, 0, 255, 100, false, true, 2, false, nil, nil, false)
+
+                if distance <= 1.5 then
+                    DrawInstructionBarNotification(location.coords.x, location.coords.y, location.coords.z - 0.15,'Appuyez sur [ ~g~E~w~ ] pour intéragir')
+
+                    if IsControlJustPressed(0, 38) then
+                        OpenLocationMenu(location.coordspawn, location.heading)
+                    end
+                end
+            end
+        end
+
+        Wait(sleep)
+    end
+end)
+
+function DrawTextFixed(x, y, text)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextScale(0.45, 0.45)
+    SetTextColour(255, 255, 255, 215)
+    SetTextDropShadow(0, 0, 0, 0, 255)
+    SetTextEdge(1, 0, 0, 0, 255)
+    SetTextDropShadow()
+    SetTextOutline()
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(x, y)
+end
---
+++ updev_location/shared/config.lua
@@ -0,0 +1,30 @@
+-----------------------------------------------------
+--                                                   -
+--     Discord: https://discord.gg/updev             -
+--                                                   -
+-----------------------------------------------------
+
+Config =  {}
+
+
+Config = {
+    LocationTime = 2, -- En minutes
+    BlipSprite = 85,
+    BlipScale = 1.0,
+    BlipColor = 3,
+    PedModel = "a_m_m_beach_01",
+    LocationVehicle = {
+        ['bmx'] = {model = "bmx", label = "BMX", price = 1000},
+        ['panto'] = {model = "panto", label = "BENEFAC PANTO", price = 1000},
+        ['faggio'] = {model = "faggio", label = "PEGASSI FAGGION", price = 1000},
+    },
+    LocationCoords = {
+        [1] = {coords = vector4(-1039.297, -2730.778, 20.214, 226.987), coordspawn = vector3(-1035.862, -2728.988, 20.075), heading =  242.293},
+        [2] = {coords = vector4(-458.874, -328.318, 34.501, 175.056), coordspawn = vector3(-463.800, -331.191, 34.363), heading = 80.719},
+        [3] = {coords = vector4(1852.122, 2582.094, 45.672, 275.226), coordspawn = vector3(1869.784, 2577.813, 45.672), heading = 268.736},
+        [4] = {coords = vector4(4540.637, -4476.005, 4.106, 130.268), coordspawn = vector3(4536.693, -4481.034, 3.759), heading = 265.658},
+        [5] = {coords = vector4(-149.893, -308.838, 38.597, 73.788), coordspawn = vector3(-148.7909, -311.025, 38.338), heading = 156.596},
+    }
+}
+
+
